6d68e737090ce89d23b9ffd8891a231ea7edb4c3
akifguldemir <akifguldemir@gmail.com>
2024-06-02 01:10:27 +0300

    minor fix

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
+use App\Middleware\AddJsonResponseHeader;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\ServerRequestCreatorFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use DI\ContainerBuilder;
-use App\Middleware\AddJsonResponseHeader;
 
 define("APP_ROOT",dirname(__DIR__));
 
@@ -45,7 +46,7 @@ $app->get('/api/posts/{id:[0-9]+}', function (Request $request, Response $respon
 
    if($product === false)
    {
-    throw new \Slim\Exception\HttpNotFoundException($request,
+    throw new HttpNotFoundException($request,
                 message: "Post couldn't found");
    }
 
