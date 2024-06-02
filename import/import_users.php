ac6d60f766902ee575d35d0a61b8347698865c27
akifguldemir <akifguldemir@gmail.com>
2024-06-02 11:54:02 +0300

    role and password added

--- import/import_users.php
+++ import/import_users.php
@@ -22,13 +22,40 @@ $json = $response->getBody()->getContents();
 $data = json_decode($json, true);
 
 foreach ($data as $item) {
+    $item['password'] = '98765';
+    $item['role'] = 'role_user';
     $userId = $userRepository->create($item);
-
     $addressId = $addressRepository->create($item['address'], $userId);
-
     $geoRepository->create($item['address']['geo'], $addressId);
-
     $companyRepository->create($item['company'], $userId);
 }
+
+$adminUser = [
+    'id'=> '11',
+    'name' => 'admin',
+    'email'=> 'admin@admin.com',
+    'username' => 'admin',
+    'password'=> 'admin12345',
+    'address'=> [
+        'street'=> 'Kulas Light',
+        'suite'=> 'Apt. 556',
+        'city'=> 'Gwenborough',
+        'zipcode'=> '92998',
+        'geo'=> [
+            'lat'=> '37.123',
+            'lng'=> '56,234'
+        ],
+    ],
+    'phone'=> '1-770-736-8031 x56442',
+    'website'=> 'wwww.test.com',
+    'company'=> [
+        'name'=> 'Romaguera',
+        'catchPhrase'=> 'Multi',
+        'bs' => 'harness '
+    ],
+    'role'=> 'role_admin'
+];
+
+$userRepository->create($adminUser);
 
 echo "Veriler başarıyla eklendi.\n";
--- src/App/Repositories/UserRepository.php
+++ src/App/Repositories/UserRepository.php
@@ -14,19 +14,36 @@ class UserRepository
 
     public function create(array $data): string
     {
-        $sql = "INSERT INTO user (id, name, username, email, phone, website) VALUES (:id, :name, :username, :email, :phone, :website)";
+        $sql = "INSERT INTO user (id, name, username, email, phone, website, password, role) VALUES (:id, :name, :username, :email, :phone, :website, :password, :role)";
 
         $pdo = $this->database->getConnection();
         $stmt = $pdo->prepare($sql);
 
-        $stmt->bindValue(":id", $data["id"], PDO::PARAM_INT);
+        $password = password_hash($data["password"], PASSWORD_BCRYPT);
+
+        if($data["id"]) $stmt->bindValue(":id", $data["id"], PDO::PARAM_INT);
         $stmt->bindValue(":name", $data["name"], PDO::PARAM_STR);
         $stmt->bindValue(":username", $data["username"], PDO::PARAM_STR);
         $stmt->bindValue(":email", $data["email"], PDO::PARAM_STR);
         $stmt->bindValue(":phone", $data["phone"], PDO::PARAM_STR);
         $stmt->bindValue(":website", $data["website"], PDO::PARAM_STR);
+        $stmt->bindValue(":password", $password, PDO::PARAM_STR);
+        $stmt->bindValue(":role", $data["role"], PDO::PARAM_STR);
 
         $stmt->execute();
         return $pdo->lastInsertId();
+    }
+
+    public function findByEmail(String $email)
+    {
+        $sql = "SELECT * FROM user WHERE email = :email;";
+
+        $pdo = $this->database->getConnection();
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->rowCount();
     }
 }
